6b356dd062b2ef2ddc80feb6944ca59ece95d312
Edson Carlos <mxzangado@gmail.com>
2025-10-15 11:06:38 -0300

    Tipos de print

---
+++ OlaMundo.py
@@ -0,0 +1,7 @@
+# Vários tipos de print's 
+nome = input('Digite seu nome: ')
+
+print (nome)
+print('Olá, Seja Bem Vindo!' +nome)           # str + variável
+print(f'Olá, Seja Bem Vindo! {nome}')         # f' str 
+print('Olá, Seja Bem vindo! {}'.format(nome)) #.format
