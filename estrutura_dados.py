d8f73e7f399aeed9f805c0dd4ddc975ae23bb2d2
EDSON (Max) <edsonpicass@gmail.com>
2025-10-17 10:01:30 -0300

    Update estrutura_dados.py

--- estrutura_dados.py
+++ estrutura_dados.py
@@ -1,5 +1,5 @@
 # Criando uma lista usando laço de repetição for.
-# recebi um quantidade especifica no for
+# recebi uma quantidade especifica no for
 lista_frutas = [input('Nome da fruta: ')for i in range(3)]
 
 print(f'Lista de frutas:{lista_frutas}')
