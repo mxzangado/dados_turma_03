d4e3af9a18094a8f3069185299be76dbfd051d67
Edson Carlos <mxzangado@gmail.com>
2025-10-17 11:14:51 -0300

    Laço de repetição

---
+++ estrutura_dados.py
@@ -0,0 +1,6 @@
+# O while executa as instruções internas ao laço enquanto a condição for verdadeira, veja a sintaxe
+#  Sim! enquanto 5 for menor igual a 9 o while vai retornar pra 6, 7 , 8 comparando até chegar no 9 10 encerra
+num = 5 
+while (num <= 9):  # O numero 5 representado por num aqui while 5 é menor ou igual a 9?
+    num = num + 1 # aqui recebe mais um para iniciar a comparação
+    print(num)   
