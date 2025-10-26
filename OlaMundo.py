28ce5d6261fb46732beda3657e50db19ddde1a76
Edson Carlos <mxzangado@gmail.com>
2025-10-26 09:58:08 -0300

    Tratamento de error

--- OlaMundo.py
+++ OlaMundo.py
@@ -1,7 +1,12 @@
-# Fazendo uma soma entre dois números
-n1 = int(input('Digite um Número: '))
-n2 = int(input('Digite Outro Número: '))
 
-soma = n1 + n2
-resultado = soma
-print(f'O resultadode {n1} + {n2} é = {resultado}')
+# Fazendo uma soma entre dois números
+try:
+    n1 = int(input('Digite um Número: '))
+    n2 = int(input('Digite Outro Número: '))
+
+    soma = n1 + n2
+    resultado = soma
+    print(f'O resultadode {n1} + {n2} é = {resultado}')
+except ValueError:
+    print("Por favor, digite apenas números inteiros.")
+
